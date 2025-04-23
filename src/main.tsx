19dfa22e0a0776d46e1c7fbd2687bbd438693cb3
Dmytro <d.salyakin@gmail.com>
2025-04-23 15:20:46 +0300

    Add TypeScript configuration and migrate files

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import 'modern-normalize';
 import App from './components/App/App';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
